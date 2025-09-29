cefdb4b03742d56f42021be006206af47bbc0b2b
Matteo Piacentini <matteo.piacentini5500@gmail.com>
2025-09-29 15:24:56 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+import { PGlite } from '@electric-sql/pglite';
+import fs from 'fs';
+
+(async () => {
+  const db = new PGlite();
+
+// Set up the DB files
+  const createTables = fs.readFileSync('create-tables.sql', 'utf8');
+  const insertData = fs.readFileSync('insert-data.sql', 'utf8');
+  await db.exec(createTables);
+  await db.exec(insertData);
+
+// Run the changes made in DM section
+  const crudOperations = fs.readFileSync('crud-operations.sql', 'utf8');
+  await db.exec(crudOperations);
+
+// Load the SQL query file
+  const query = fs.readFileSync('query.sql', 'utf8');
+
+// Run the query from the query file
+  const response = await db.query(query);
+
+  console.clear();
+  console.table(response.rows);
+})();
